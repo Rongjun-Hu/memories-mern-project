ba141fdbf3ce00cd7863b80cfd2e027245711000
RongjunHu <rongjun.hu@student.uts.edu.au>
2020-11-06 13:21:33 +1100

    connected to mongodb and express

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+
+import postRoutes from "./routes/posts.js"
+
+
+
+const app = express();
+
+// express middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+// Routes
+app.use('/posts', postRoutes)
+
+// MongoDB connection
+const CONNECTION_URL = "mongodb+srv://peter:Pr@716729@memories-mern.hkmhf.mongodb.net/memory?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5001
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+	.catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
