914aafd59aef78b7f949f39f81a99592f7bc82df
RongjunHu <rongjun.hu@student.uts.edu.au>
2020-11-09 09:31:10 +1100

    dispatch action using useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from "react-redux"
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
+import { getPosts } from "./actions/posts"
 import memories from "./images/memories.png"
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -7,6 +9,11 @@ import useStyles from "./styles"
 
 const App = () => {
 	const classes = useStyles()
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(getPosts);
+	}, [dispatch])
 
 	return (
 		<Container maxwidth="lg">
@@ -16,7 +23,7 @@ const App = () => {
 			</AppBar>
 			<Grow in>
 				<Container>
-					<Grid container justify="space-betweeb" alignItems="stretch" spacing={3}>
+					<Grid container justify="space-between" alignItems="stretch" spacing={3}>
 						<Grid item xs={12} sm={7}>
 							<Posts />
 						</Grid>
