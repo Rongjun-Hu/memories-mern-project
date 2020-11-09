cf82dc8c857aceecf5b33350fcf9c6b611bd77aa
RongjunHu <rongjun.hu@student.uts.edu.au>
2020-11-09 13:51:08 +1100

    post now can update with the currentId

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import Post from '../models/Post.js'
 
 export const getPosts = async (req, res) => {
@@ -19,4 +20,15 @@ export const createPost = async (req, res) => {
 	} catch (error) {
 		res.status(409).json({ message: error.message })
 	}
+}
+
+export const updatePost = async (req, res) => {
+	const { id: _id } = req.params
+	const post = req.body
+
+	if (Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+	const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true })
+
+	res.json(updatedPost)
 }
