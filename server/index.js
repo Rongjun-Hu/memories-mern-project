3abab32740c18225cbba4469108a79ef4ed0b52a
RongjunHu <rongjun.hu@student.uts.edu.au>
2020-11-09 11:00:02 +1100

    ..

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ import cors from "cors"
 
 import postRoutes from "./routes/posts.js"
 
-
-
 const app = express();
 
 // express middleware
@@ -18,11 +16,9 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 // MongoDB connection
-const CONNECTION_URL = "mongodb+srv://peter:Pr@716729@memories-mern.hkmhf.mongodb.net/memory?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://peter:Pr@716729@memories-mern.hkmhf.mongodb.net/memory"
 const PORT = process.env.PORT || 5001
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
 	.catch((error) => console.log(error.message))
-
-mongoose.set('useFindAndModify', false);
