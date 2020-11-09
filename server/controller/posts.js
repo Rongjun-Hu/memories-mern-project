5157df1145fa84a8c01047eb97e41dfcb0a47425
RongjunHu <rongjun.hu@student.uts.edu.au>
2020-11-09 14:14:57 +1100

    now can update post

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -26,9 +26,9 @@ export const updatePost = async (req, res) => {
 	const { id: _id } = req.params
 	const post = req.body
 
-	if (Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+	if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with that id`)
 
-	const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true })
+	const updatedPost = await Post.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
 
 	res.json(updatedPost)
 }
