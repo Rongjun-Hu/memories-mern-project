d05229e83ac46f09f81518fe2f37c702a553d460
Peter <rongjun.hu.0716@gmail.com>
2021-06-26 00:52:40 +1000

    add search route and controller

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -10,6 +10,41 @@ export const getPosts = async (req, res) => {
   }
 };
 
+// export const getPostsBySearch = async (req, res) => {
+//   const { searchQuery, tags } = req.query;
+
+//   try {
+//     const title = new RegExp(searchQuery, "i");
+
+//     // Find all the posts that match one of those
+//     // Is one of the tags in the array of tags === tags
+//     const posts = await Post.find({
+//       $or: [{ title }, { tags: { $in: tags.split(",") } }],
+//     });
+
+//     res.json({ data: posts });
+//   } catch (error) {
+//     res.status(404).json({ message: error.message });
+//     console.log(error);
+//   }
+// };
+
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, "i");
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const createPost = async (req, res) => {
   const post = req.body;
   const newPost = new Post({
