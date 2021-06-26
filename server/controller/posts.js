5989f3c6c9a4a09b99eda686c92785510760fd36
Peter <rongjun.hu.0716@gmail.com>
2021-06-26 22:58:54 +1000

    Get post route and controller

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -2,32 +2,36 @@ import mongoose from "mongoose";
 import Post from "../models/Post.js";
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // Get the starting index of every page
+    const total = await Post.countDocuments({});
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-// export const getPostsBySearch = async (req, res) => {
-//   const { searchQuery, tags } = req.query;
+export const getPost = async (req, res) => {
+  const { id } = req.params;
 
-//   try {
-//     const title = new RegExp(searchQuery, "i");
+  try {
+    const post = await Post.findById(id);
 
-//     // Find all the posts that match one of those
-//     // Is one of the tags in the array of tags === tags
-//     const posts = await Post.find({
-//       $or: [{ title }, { tags: { $in: tags.split(",") } }],
-//     });
-
-//     res.json({ data: posts });
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//     console.log(error);
-//   }
-// };
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getPostsBySearch = async (req, res) => {
   const { searchQuery, tags } = req.query;
