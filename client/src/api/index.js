b87cead203c8ac63830674fe0a3fba4c22ccd2aa
Peter <51101346+Rongjun-Hu@users.noreply.github.com>
2022-02-25 21:55:08 +1100

    fix cors issue

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,38 +1,36 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://memory-crud-auth.herokuapp.com",
-  headers: {
-    'Access-Control-Allow-Origin': 'https://memories-crud-auth.netlify.app'
-  }
-  // baseURL: "http://localhost:5000",
-  // https://memories-crud-auth.herokuapp.com/posts/search?searchQuery=qwe&tags=
+	baseURL: "https://memory-crud-auth.herokuapp.com",
+	// baseURL: "http://localhost:5000",
+	headers: {
+		'Access-Control-Allow-Origin': '*'
+	}
+	// https://memories-crud-auth.herokuapp.com/posts/search?searchQuery=qwe&tags=
 });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile")).token
-    }`;
-  }
+	if (localStorage.getItem("profile")) {
+		req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
+			}`;
+	}
 
-  return req;
+	return req;
 });
 
 export const fetchPost = (id) => API.get(`/posts/${id}`);
 export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 export const fetchPostsBySearch = (searchQuery) =>
-  API.get(
-    `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${
-      searchQuery.tags
-    }`
-  );
+	API.get(
+		`/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${searchQuery.tags
+		}`
+	);
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
-  API.patch(`/posts/${id}`, updatedPost);
+	API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const comment = (value, id) =>
-  API.post(`/posts/${id}/commentPost`, { value });
+	API.post(`/posts/${id}/commentPost`, { value });
 export const signIn = (formData) => API.post("/user/signin", formData);
 export const signUp = (formData) => API.post("/user/signup", formData);
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,28 +5,27 @@ import { useSelector } from "react-redux";
 import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
-  const classes = useStyles();
-  const { posts, isLoading } = useSelector((state) => state.posts);
+	const classes = useStyles();
+	const { posts, isLoading } = useSelector((state) => state.posts);
 
-  if (!posts.length && !isLoading) return "No posts";
-  console.log(posts);
+	if (!posts.length && !isLoading) return "No posts";
 
-  return isLoading ? (
-    <CircularProgress />
-  ) : (
-    <Grid
-      className={classes.container}
-      container
-      alignItems="stretch"
-      spacing={3}
-    >
-      {posts?.map((post) => (
-        <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
-          <Post post={post} setCurrentId={setCurrentId} />
-        </Grid>
-      ))}
-    </Grid>
-  );
+	return isLoading ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems="stretch"
+			spacing={3}
+		>
+			{posts?.map((post) => (
+				<Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+					<Post post={post} setCurrentId={setCurrentId} />
+				</Grid>
+			))}
+		</Grid>
+	);
 };
 
 export default Posts;
